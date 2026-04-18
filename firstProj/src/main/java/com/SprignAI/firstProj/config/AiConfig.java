aa0f5498a74970f76a55c9897ef828ba74658ad2
shrey200634 <dshrey60@gmail.com>
2026-04-19 01:32:16 +0530

    add multiple ai model in service .

---
+++ firstProj/src/main/java/com/SprignAI/firstProj/config/AiConfig.java
@@ -0,0 +1,23 @@
+package com.SprignAI.firstProj.config;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.google.genai.GoogleGenAiChatModel;
+import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AiConfig {
+
+
+    @Bean("openAIChatClint")
+    public ChatClient openAiChatModel(OpenAiChatModel openAiChatModel){
+      return ChatClient.builder(openAiChatModel).build();
+
+    }
+    @Bean("GoogleGenChatAI")
+    public ChatClient googleGenAiChatModel(GoogleGenAiChatModel googleGenAiChatModel){
+        return ChatClient.builder(googleGenAiChatModel).build();
+
+    }
+}
--- firstProj/src/main/java/com/SprignAI/firstProj/controller/ChatController.java
+++ firstProj/src/main/java/com/SprignAI/firstProj/controller/ChatController.java
@@ -1,6 +1,7 @@
 package com.SprignAI.firstProj.controller;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,20 +15,28 @@ import org.springframework.ai.google.genai.GoogleGenAiChatModel;
 public class ChatController
 {
 
-    private final ChatClient groqChatClient;
-    private final ChatClient geminiChatClient;
+    private ChatClient openAiChatModel;
+    private ChatClient geminiChatClient;
 
-    public ChatController(OpenAiChatModel openAiChatModel, GoogleGenAiChatModel geminiChatModel) {
+    public  ChatController(@Qualifier("openAIChatClint") ChatClient openAiChatModel ,
+                           @Qualifier("GoogleGenChatAI") ChatClient geminiChatClient){
+        this.geminiChatClient=geminiChatClient;
+        this.openAiChatModel=openAiChatModel;
 
-        // 2. We assign the builder to those EXACT SAME variables here
-        this.groqChatClient = ChatClient.builder(openAiChatModel).build();
-        this.geminiChatClient = ChatClient.builder(geminiChatModel).build();
+
     }
+
+//    public ChatController(OpenAiChatModel openAiChatModel, GoogleGenAiChatModel geminiChatModel) {
+//
+//        // 2. We assign the builder to those EXACT SAME variables here
+//        this.groqChatClient = ChatClient.builder(openAiChatModel).build();
+//        this.geminiChatClient = ChatClient.builder(geminiChatModel).build();
+//    }
 
     @GetMapping("/chat")
     public ResponseEntity<String> chat(@RequestParam(value = "q", required = true) String q) {
 
-        var resultResponse=this.geminiChatClient
+        var resultResponse=this.openAiChatModel
                 .prompt(q).call().content();
         return ResponseEntity.ok(resultResponse);
     }
