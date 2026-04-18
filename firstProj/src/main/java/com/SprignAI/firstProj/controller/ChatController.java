420a562985b8a122d69804792861345d418d5632
shrey200634 <dshrey60@gmail.com>
2026-04-18 17:02:15 +0530

    add multiple ai model in ai service in spring boot

--- firstProj/src/main/java/com/SprignAI/firstProj/controller/ChatController.java
+++ firstProj/src/main/java/com/SprignAI/firstProj/controller/ChatController.java
@@ -6,22 +6,29 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.ai.google.genai.GoogleGenAiChatModel;
 
 @RestController
 @RequestMapping
 public class ChatController
 {
 
-    private ChatClient chatClient;
+    private final ChatClient groqChatClient;
+    private final ChatClient geminiChatClient;
 
-    public  ChatController(ChatClient.Builder builder){
-        this.chatClient=builder.build();
+    public ChatController(OpenAiChatModel openAiChatModel, GoogleGenAiChatModel geminiChatModel) {
+
+        // 2. We assign the builder to those EXACT SAME variables here
+        this.groqChatClient = ChatClient.builder(openAiChatModel).build();
+        this.geminiChatClient = ChatClient.builder(geminiChatModel).build();
     }
 
     @GetMapping("/chat")
     public ResponseEntity<String> chat(@RequestParam(value = "q", required = true) String q) {
 
-        var resultResponse=chatClient.prompt(q).call().content();
+        var resultResponse=this.geminiChatClient
+                .prompt(q).call().content();
         return ResponseEntity.ok(resultResponse);
     }
 }
